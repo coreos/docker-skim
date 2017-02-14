3949bfbb1a3aa875a57768e40ef1c3de35d8d661
Chris Baumbauer <christopher.baumbauer@coreos.com>
2017-02-14 15:52:37 -0800

    Added a .gitignore file and updated build.sh to clean out build artifacts

--- build.sh
+++ build.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
+set -e
+
 BINS="enter gc run stop"
 BASEDIR=$PWD
 
 # Verify the core binaries of go and glide exist
+GLIDEBIN=`which glide`
+GOBIN=`which go`
+
+if [ "x$GLIDEBIN" == "x" ]; then
+    echo "Glide needs to be in your path"
+    exit 254
+fi
+
+if [ "x$GOBIN" == "x" ]; then
+    echo "The go binary needs to be in your path"
+    exit 254
+fi
+
+# Clean the repo, but save the vendor area
+if [ "x$1" != "x" ] && [ "clean" == $1 ]; then
+    echo "cleaning project"
+    rm -rf target
+    rm stage1-skim.aci
+    rm aci/actool
+
+    for i in $BINS; do
+        rm $i/$i
+    done
+
+    exit 0
+fi
 
 # Ensure the vendor directory exists
 if [ ! -d vendor ]; then
-    glide install
+    $GLIDEBIN install
 
     # Remove the vendor area from github.com/coreos/rkt since this will cause
     # a cyclic dependency with run/run.go
@@ -16,7 +44,7 @@ fi
 
 # Build the actool
 pushd vendor/github.com/appc/spec/actool
-go build
+$GOBIN build
 mv actool $BASEDIR/aci
 popd
 
@@ -29,11 +57,16 @@ if [ ! -d target ]; then
 fi
 
 for i in $BINS; do
-    pushd $i && go build
+    pushd $i && $GOBIN build
     cp $i ../target/rootfs
     popd
 done
 
 # Generate the aci image
 cp aci/aci-manifest.in target/manifest
+
+if [ -f stage1-skim.aci ]; then
+    rm stage1-skim.aci
+fi
+
 aci/actool build target stage1-skim.aci
