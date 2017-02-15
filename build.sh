b1c1dc68ed32cac7811a9a59f799151d070d5733
Chris Baumbauer <christopher.baumbauer@coreos.com>
2017-02-14 16:50:41 -0800

    Added a copyright block and added -f to the rm commands to keep them from erroring out

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Copyright 2017 The rkt Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 set -e
 
 BINS="enter gc run stop"
@@ -23,11 +38,11 @@ fi
 if [ "x$1" != "x" ] && [ "clean" == $1 ]; then
     echo "cleaning project"
     rm -rf target
-    rm stage1-skim.aci
-    rm aci/actool
+    rm -f stage1-skim.aci
+    rm -f aci/actool
 
     for i in $BINS; do
-        rm $i/$i
+        rm -f $i/$i
     done
 
     exit 0
